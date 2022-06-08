d1e3ef431fae00b01bee9967d9e68421670afaa9
technolink.github.io <106021247+huseyneliyevtekno@users.noreply.github.com>
2022-06-08 15:06:50 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,17 +3,19 @@
 /* Bildirimler funksiya Javascript */
 
 
+var box = document.querySelector('.notification-box');
+var down = false;
+
+
+
 function notification(){
-    var box = document.querySelector('.notification-box')
-
-    if(box.style.display === "none") {
-        box.style.display = "block";
-    }
-
-    else{
-        box.style.display = "none";
+    if (down) {
+        box.style.height = '0px';
+        box.style.opacity = 0;
+        down = false;
+    }else{
+        box.style.height = '320px';
+        box.style.opacity = 1;
+        down = true;
     }
 }
-
-
-
